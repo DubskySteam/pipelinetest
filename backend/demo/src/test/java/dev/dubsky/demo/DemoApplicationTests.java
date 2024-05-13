443e4f14bae441acdf747ca86af7b447097e32ca
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-05-13 13:09:01 +0200

    init

---
+++ backend/demo/src/test/java/dev/dubsky/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dubsky.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
